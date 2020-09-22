71126907d5d075567c633c4d6f99fd76e691c8ec
meydan kaa <meydank@moveo.co.il>
2020-09-22 14:52:11 +0300

    chnage app

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const mondayData = require('./libs/monday');
+const dotenv = require('dotenv').config();
 const fetch = require("node-fetch");
 const config = require('./config');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
 
 
 // Get Memberd & Houers data from sprint-board
-let getMemberd_getHouers = 'query{boards(ids: 671223520){ items { column_values(ids:["people3", "retainer_billing8"]){ text }}}}'
+let getMemberd_getHouers = 'query {boards(ids: 671223520){ items { column_values(ids:["people3", "retainer_billing8"]){ text }}}}'
 
 // Get members by they id from Info-board
-let getMembersById = 'query{ boards(ids:667708556){ items { id name }}}'
+let getMembersById = 'query {boards(ids:667708556){ items { id name }}}'
 async function query(method, queryType, queryString) {
   return fetch("https://api.monday.com/v2", {
     method: method,
@@ -25,7 +29,21 @@ async function query(method, queryType, queryString) {
 
 fetchAndMutation()
 
+// Check app conntion
+// alert("Hello! Func fan run Now!!");
+changeTextIntegrtion();
+console.log('none ==>');
+
+function changeTextIntegrtion(){
+  console.log('Server is runing on monday app integrtion...');
+  let updateFields = 'mutation {change_column_value(board_id: 728270613, item_id: 730680397, column_id: january, value:"12020") {id}}';
+ 
+  query('post', 'query', updateFields);
+}
+//---------
+
 async function fetchAndMutation() {
+  console.log('Server is runing on monday app integrtion...');
   let response = await query('post', 'query', getMemberd_getHouers);
   boards = response.data.boards;
 
@@ -49,13 +67,18 @@ async function fetchAndMutation() {
   
   let boardInfo = await query('post', 'query', getMembersById)
   let empIds = boardInfo.data.boards[0].items;
-
   empIds.forEach(item => { newObj[item.name] = item.id }) ;
 
   empArray.forEach(item => {
     let allMembers = item.column_values[0].text;
     let allhouers = item.column_values[1].text;
-    let mutationFields = `mutation {change_column_value(board_id: 667708556, item_id: ${newObj[allMembers]}, column_id: hours_tracked, value:"${allhouers}") {id}}`;
+    let mutationFields = `mutation {change_column_value(board_id: 667708556, item_id: ${newObj[allMembers]}, column_id: hours_tracked, value:"${allhouers}"){ id }}`;
     query('post', 'query', mutationFields)
   })
 };
+
+app.listen(port, () => console.log(`Quickstart app listening at http://localhost:${port}`))
+
+module.exports = app;
+//numbers60 - Source coulmn
+//january - 2 Salary Base
